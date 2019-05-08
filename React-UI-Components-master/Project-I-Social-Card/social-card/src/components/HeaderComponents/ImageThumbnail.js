a4a1b9e126cd766a02446cb7d0d666dd0140999f
Dil Sombayeva <dilyasom@gmail.com>
2019-05-08 10:05:55 -0700

    simple test

--- React-UI-Components-master/Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ React-UI-Components-master/Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const LogoL = () => {
+ return (
+  <div>
+   <img src="../../../public/favicon.ico" alt="logo" />
+  </div>
+ );
+};
+
+export default LogoL;
